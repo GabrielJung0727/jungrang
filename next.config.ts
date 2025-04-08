13b9ac2df93ce224f44cfc193412fed30882d4bc
GabrielJung <98318273+GabrielJung0727@users.noreply.github.com>
2025-04-08 14:37:01 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: '/jungtrio',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
